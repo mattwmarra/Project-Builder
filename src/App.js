a62441368f0eeb54f191a97254f365131d80a072
Matt <matt.marranara@gmail.com>
2020-08-04 14:41:19 -0700

    Connected to server successfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
-import { Card, Container, Col, Row } from 'react-bootstrap';
+import React, {useState, useEffect} from 'react';
+import { Card, Container, Col, Row, Button } from 'react-bootstrap';
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 import uuid from 'uuid/v4';
+import { get } from 'jquery';
+const axios = require('axios')
 
-const itemsFromBackend = [
+let itemsFromBackend = [
   {id: uuid(), content: 'First Task'},
   {id: uuid(), content: 'Second Task'}
 ]
@@ -33,7 +35,6 @@ const onDragEnd = (result, columns, setColumns) => {
     const destItems = [...destColumn.items];
     const [removed] = sourceItems.splice(source.index, 1);
     destItems.splice(destination.index, 0, removed);
-    console.log(sourceColumn)
     setColumns({
       ...columns,
       [source.droppableId]: {
@@ -45,6 +46,7 @@ const onDragEnd = (result, columns, setColumns) => {
         items: destItems
       }
     })
+
   }
   else {
     const column = columns[source.droppableId];
@@ -61,8 +63,48 @@ const onDragEnd = (result, columns, setColumns) => {
   }
 }
 
+const getTasksFromDatabase = () => {
+  axios.get('/api', {
+
+  })
+  .then((res) => {
+    //handle success
+    
+    let newItems = res.data.tasks;
+    itemsFromBackend = newItems;
+    console.log(res.data.tasks);
+  })
+  .catch((err)=>{
+    //handle failure
+    console.log("Error: ", err)
+  })
+}
+
+const addTaskToColumn = (columns, setColumns, id) => {
+  const column = columns[id];
+  const newItem = {
+    id:uuid(),
+    content: "Next Task"
+  }
+  const columnItems = [...column.items];
+  columnItems.splice(column.index, 0, newItem);
+  setColumns({
+    ...columns,
+    [id] : {
+      ...column,
+      items: columnItems
+    }
+  })
+  
+}
+
 function App() {
   const [columns, setColumns] = useState(columnsFromBackend);
+
+  useEffect(() => {
+    getTasksFromDatabase()
+  
+  });
 
   return (
     <div className="App">
@@ -72,18 +114,20 @@ function App() {
           <Row>
             {Object.entries(columns).map(([id, column]) => {
               return (
-
                   <Droppable droppableId={id} key={id} style={{margin: 8}}>
                   {(provided, snapshot) => {
                     return(
                       <div>
-                        <h2>{column.name}</h2>
+                        <div style={{display:"flex"}}>
+                            <h2>{column.name}</h2>
+                            <Button size="sm" key={id} onClick={result => addTaskToColumn(columns, setColumns, id)}>Add Task</Button>
+                        </div>
                       <Col {...provided.droppableProps}
                         ref={provided.innerRef}
                         style={{
                           background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
                           padding: 4,
-                          margin: 16,
+                          margin: 8,
                           width: 250,
                           minHeight: 500
                         }}
@@ -94,19 +138,11 @@ function App() {
                               <Draggable key={item.id} draggableId={item.id} index={index}>
                                 {(provided, snapshot) => {
                                   return (
-                                    <Card
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        style={{userSelect:'none',
-                                        margin: 8,
-                                        backgroundColor : snapshot.isDragging ? "#0984e3" : "#dfe6e9",
-                                        ...provided.draggableProps.style
-                                      }}
-                                    >
-                                      <Card.Title>{item.content}</Card.Title>
-                                      <Card.Text>Details</Card.Text>
-                                      </Card>
+                                    <Category title={item.title} 
+                                              content={item.content} 
+                                              provided={provided} 
+                                              snapshot={snapshot}>
+                                      </Category>
                                   )
                                 }}
                               </Draggable>
@@ -132,10 +168,18 @@ const Category = (props) => {
 
   return(
     <div>
-      <Card>
+      <Card 
+          ref={props.provided.innerRef}
+          {...props.provided.draggableProps}
+          {...props.provided.dragHandleProps}
+          style={{userSelect:'none',
+          margin: 8,
+          backgroundColor : props.snapshot.isDragging ? "#0984e3" : "#dfe6e9",
+          ...props.provided.draggableProps.style
+        }}>
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
-          <Card.Text></Card.Text>
+          <Card.Text>{props.content}</Card.Text>
         </Card.Body>
       </Card> 
     </div>
