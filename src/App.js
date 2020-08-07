0f7c9cf39fdcebdce0bc4bbf1ccfca5244216034
Matt <matt.marranara@gmail.com>
2020-08-07 15:09:00 -0700

    canban board works, can add tasks to server now

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import { Card, Container, Col, Row, Button } from 'react-bootstrap';
+import { Card, Container, Col, Row, Button, Popover, Form, OverlayTrigger } from 'react-bootstrap';
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
-import uuid from 'uuid/v4';
-import { get } from 'jquery';
 const axios = require('axios')
 
-let columnsFromBackend = {
-    // "first" : {
-    //   name : 'Requested',
-    //   tasks: [{
-    //     _id : "123",
-    //     title: "djksahd",
-    //     content: "dkjhsad"
-    //   }]
-    // },
-    // [uuid()] : {
-    //   name: 'In Progress',
-    //   tasks : []
-    // },
-    // [uuid()] : {
-    //   name: 'Done',
-    //   tasks: []
-    // }
-};
-//5f29b201af027a1c9490f480
-// 5f29b207af027a1c9490f481
 const onDragEnd = (result, columns, setColumns) => {
   if(!result.destination) return;
   const {source, destination} = result;
@@ -33,8 +11,17 @@ const onDragEnd = (result, columns, setColumns) => {
     const destColumn = columns[destination.droppableId];
     const sourceItems = [...sourceColumn.tasks];
     const destItems = [...destColumn.tasks];
-    const [removed] = sourceItems.splice(source.index, 1);
+    let [removed] = sourceItems.splice(source.index, 1);
+
+    removed.parent = destination.droppableId
     destItems.splice(destination.index, 0, removed);
+    axios.post('/updateTasks', {
+      updatedTask : removed
+    }).then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    });
     setColumns({
       ...columns,
       [source.droppableId]: {
@@ -46,6 +33,7 @@ const onDragEnd = (result, columns, setColumns) => {
         tasks: destItems
       }
     })
+
 
   }
   else {
@@ -63,30 +51,25 @@ const onDragEnd = (result, columns, setColumns) => {
   }
 }
 
-const getTasksFromDatabase = (columns, setColumns) => {
+const getTasksFromDatabase = ( setColumns) => {
   axios.get('/api', {
   })
   .then((res) => {
     let [data] = res.data;
-    console.log(data);
+    console.log(res.data)
     const dataColumns = data.columns;
-    console.log(dataColumns)
     let newState = {};
-
+    console.log({data})
     for(const key of dataColumns){
-      let x = data.tasks.filter(obj => {
-        return (obj.parent === key._id);
-      })
-      console.log({x})
       newState[key._id] = {
         _id : key._id,
         name : key.name,
-        tasks : data.tasks.filter(obj => {return (obj.parent === key._id);})
+        tasks : data.tasks.filter(obj => {return (obj.parent === key._id);}) //jankey fix, should be better once I learn aggregation better
         //pull the tasks from tasks array by using the reference keys in the column.tasks array
       }
     }
-    console.log(newState)
     setColumns(newState)
+  
   })
 
   .catch((err)=>{
@@ -95,31 +78,70 @@ const getTasksFromDatabase = (columns, setColumns) => {
   })
 }
 
-const addTaskToColumn = (columns, setColumns, _id) => {
-
+const addTaskToColumn = (columns, setColumns, _id, values) => {
   const column = columns[_id];
-  const newItem = {
-    _id: uuid(),
-    title : "Another Task!",
-    content: "Added Text"
-  }
-  const columnItems = [...column.tasks];
-  columnItems.splice(column.index, 0, newItem);
-  setColumns({
-    ...columns,
-    [_id] : {
-      ...column,
-      tasks: columnItems
-    }
+
+  axios.post("/addTask", {
+    title: values.title,
+    content : values.description,
+    parentID : _id
+  }).then((res) =>{
+    console.log(res.data)
+    // const newItem = {
+    //   _id: res.data._id,
+    //   title : res.data.title,
+    //   content: res.data.content,
+    //   parent: res.data.parent
+    // }
+    const newItem = res.data
+    const columnItems = [...column.tasks];
+    columnItems.splice(column.index, 0, newItem);
+    setColumns({
+      ...columns,
+      [_id] : {
+        ...column,
+        tasks: columnItems
+      }
+    })
   })
 }
 
+const EditCardPopover = (props) => {
+   const [values, setValues] = useState({
+    title : "",
+    description : ""
+  })
+
+  console.log(props.columns)
+
+  return(
+      <div>
+        <Popover>
+          <Popover.Title>Add Task</Popover.Title>
+          <Popover.Content>
+            <Form>
+              <Form.Text>Task Title</Form.Text>
+              <Form.Control id="title" onChange={(event) => setValues({
+                title : event.target.value,
+                description : values.description
+                })} value={values.title}></Form.Control>
+              <Form.Text>Task Description</Form.Text>
+              <Form.Control id="description" placeholder="(Optional)" onChange={(event) => setValues(
+                { title: values.title,
+                  description : event.target.value})}></Form.Control>
+              <Button onClick={() => addTaskToColumn(props.columns, props.setColumns, props._id, values, setValues)}>Add</Button>
+            </Form>
+          </Popover.Content>
+        </Popover>
+      </div>
+  )
+}
+
 function App() {
-  const [columns, setColumns] = useState(columnsFromBackend);
+  const [columns, setColumns] = useState([]);
 
   useEffect(() => {
-    getTasksFromDatabase(columns, setColumns)
-
+    getTasksFromDatabase(setColumns)
   }, []);
   //[] is a currently empty list of values to check for changes of
   // this keeps the request from being repeatedly sent
@@ -134,19 +156,24 @@ function App() {
               return (
                   <Droppable droppableId={_id} key={_id} style={{margin: 8}}>
                   {(provided, snapshot) => {
+                    console.log({provided})
                     return(
                       <div>
                         <div style={{display:"flex"}}>
                             <h3>{column.name}</h3>
-                            <Button size="sm" key={_id} onClick={() => addTaskToColumn(columns, setColumns, _id)}>Add Task</Button>
+                            <OverlayTrigger trigger="click" overlay={<EditCardPopover />} placement="right" //can only use hooks "nested" by passing as a <Element/> idk why
+                                            columns={columns} setColumns={setColumns} _id={_id}>
+                              <Button size="sm" key={_id} >Add Task</Button>
+                            </OverlayTrigger>
+
                         </div>
                       <Col {...provided.droppableProps}
                         ref={provided.innerRef}
                         style={{
                           background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
                           padding: 4,
-                          margin: 8,
-                          width: 250,
+                          width: 300,
+                          margin: 16,
                           minHeight: 500
                         }}
                         >
@@ -156,8 +183,8 @@ function App() {
                               <Draggable key={task._id} draggableId={task._id} index={index}>
                                 {(provided, snapshot) => {
                                   return (
-                                    <Category title={task.title} 
-                                              content={task.content} 
+                                    <Category 
+                                              task={task}
                                               provided={provided} 
                                               snapshot={snapshot}>
                                       </Category>
@@ -182,26 +209,29 @@ function App() {
   );
 }
 
-const Category = (props) => {
 
+
+const Category = (props) => {
   return(
     <div>
-      <Card 
+      <Card
+          id={props.task.id}
           ref={props.provided.innerRef}
           {...props.provided.draggableProps}
           {...props.provided.dragHandleProps}
           style={{userSelect:'none',
           margin: 8,
-          backgroundColor : props.snapshot.isDragging ? "#0984e3" : "#dfe6e9",
+          backgroundColor : "#fff",
+          // boxShadow: props.snapshot.isDragging ? "10px 10px 5px -4px rgba(97,97,97,0.37)" : "none",
           ...props.provided.draggableProps.style
         }}>
         <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text>{props.content}</Card.Text>
+          <Card.Title>{props.task.title}</Card.Title>
+          <Card.Subtitle>{props.task.lastMovedDate !== undefined ? props.task.lastMovedDate : "N/A"}</Card.Subtitle>
+          <Card.Body>Details: {props.task.content}</Card.Body>
         </Card.Body>
       </Card> 
     </div>
-
   )
 }
 
